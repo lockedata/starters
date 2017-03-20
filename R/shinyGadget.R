f3ba01bac81a57c717a78ade927e82c9ba2dbea8
Steph Locke <steph@itsalocke.com>
2017-03-20 17:43:43 +0000

    fully namespaced gadget

--- R/shinyGadget.R
+++ R/shinyGadget.R
@@ -1,32 +1,29 @@
-library(shiny)
-library(miniUI)
-
 basicProjectGadget <- function() {
 
-ui <- miniPage(
-  gadgetTitleBar("Create Basic pRoject"),
-  miniContentPanel(
-    inputPanel(
-      textInput("name", "Project Name")
+ui <- miniUI::miniPage(
+  miniUI::gadgetTitleBar("Create Basic pRoject"),
+  miniUI::miniContentPanel(
+    shiny::inputPanel(
+      shiny::textInput("name", "Project Name")
     ),
-    inputPanel(
-      checkboxInput("readme", "Create README", value = TRUE),
-      checkboxInput("git", "Use Git", value = TRUE),
-      checkboxInput("travis", "Use Travis", value = TRUE),
-      checkboxInput("packrat", "Use Packrat", value = TRUE)
+    shiny::inputPanel(
+      shiny::checkboxInput("readme", "Create README", value = TRUE),
+      shiny::checkboxInput("git", "Use Git", value = TRUE),
+      shiny::checkboxInput("travis", "Use Travis", value = TRUE),
+      shiny::checkboxInput("packrat", "Use Packrat", value = TRUE)
     )
   )
 )
 
 server <- function(input, output, session) {
 
-  observeEvent(input$done, {
+  shiny::observeEvent(input$done, {
     createBasicProject(name = input$name, readme = input$readme,
                        git = input$git, travis = input$travis,
                        packrat = input$packrat)
-    stopApp()
+    shiny::stopApp()
   })
 }
 
-runGadget(ui, server)
+shiny::runGadget(ui, server)
 }
