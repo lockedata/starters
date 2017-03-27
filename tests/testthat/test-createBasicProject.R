4127832a6cb53e945203eece1a66c0f725b08f75
jonmcalder <jonmcalder@gmail.com>
2017-03-28 00:08:18 +0200

    Add tests to check that files/folders are created as expected for default function calls

--- tests/testthat/test-createBasicProject.R
+++ tests/testthat/test-createBasicProject.R
@@ -1,5 +1,20 @@
 context("createBasicProject")
 
-test_that("createBasicProject() creates as expected when using defaults",{
+project_name <- "basicProject"
+
+test_that("createBasicProject() creates as expected when using defaults", {
+
+  createBasicProject(project_name)
+
+  expect_true(file.exists(file.path(project_name, paste0(project_name, ".Rproj"))))
+  expect_true(file.exists(file.path(project_name, "DESCRIPTION")))
+  expect_true(file.exists(file.path(project_name, "R")))
+  expect_true(file.exists(file.path(project_name, "README.Rmd")))
+  expect_true(file.exists(file.path(project_name, "packrat")))
+  expect_true(file.exists(file.path(project_name, ".git")))
+  expect_true(file.exists(file.path(project_name, ".gitignore")))
+  expect_true(file.exists(file.path(project_name, ".travis.yml")))
 
 })
+
+unlink(project_name, recursive = TRUE, force = TRUE)
--- tests/testthat/test-createPackageProject.R
+++ tests/testthat/test-createPackageProject.R
@@ -1,5 +1,26 @@
 context("createPackageProject")
 
+project_name <- "packageProject"
+
 test_that("createPackageProject() creates as expected when using defaults",{
 
+  createPackageProject(project_name)
+
+  expect_true(file.exists(file.path(project_name, paste0(project_name, ".Rproj"))))
+  expect_true(file.exists(file.path(project_name, "codecov.yml")))
+  expect_true(file.exists(file.path(project_name, "CONDUCT.md")))
+  expect_true(file.exists(file.path(project_name, "DESCRIPTION")))
+  expect_true(file.exists(file.path(project_name, "LICENSE")))
+  expect_true(file.exists(file.path(project_name, "NAMESPACE")))
+  expect_true(file.exists(file.path(project_name, "NEWS.md")))
+  expect_true(file.exists(file.path(project_name, "R")))
+  expect_true(file.exists(file.path(project_name, "README.Rmd")))
+  expect_true(file.exists(file.path(project_name, "tests")))
+  expect_true(file.exists(file.path(project_name, "vignettes")))
+  expect_true(file.exists(file.path(project_name, ".git")))
+  expect_true(file.exists(file.path(project_name, ".gitignore")))
+  expect_true(file.exists(file.path(project_name, ".travis.yml")))
+
 })
+
+unlink(project_name, recursive = TRUE, force = TRUE)
