1cf5ecea77ba9976d29e6653d93477b6b2f3c50e
Steph <Steph@itsalocke.com>
2017-09-23 10:51:12 +0100

    Fix packrat var dep This also improves additional package handling and removing a package dep :tada:

--- R/createTrainingProject.R
+++ R/createTrainingProject.R
@@ -24,11 +24,12 @@ createTrainingProject <- function(name,
   handoutEngine <- match.arg(handoutEngine, c("rmarkdown", "bookdown", "tufte"))
   slideEngine <- match.arg(slideEngine, c("rmarkdown", "revealjs", "xaringan"))
 
-  # Installation dir
-  usePackrat <- !methods::hasArg("packrat")
-  if (!usePackrat)
-    usePackrat <- list(...)[["packrat"]]
-  installDir <- ifelse(usePackrat, name, .libPaths())
+  if(!requireNamespace(handoutEngine, quietly = TRUE)){
+    stop("You need to install ",handoutEngine, " first")
+  }
+  if(!requireNamespace(slideEngine, quietly = TRUE)){
+    stop("You need to install ",slideEngine, " first")
+  }
 
   # Skeleton
   message("Creating skeleton")
@@ -37,7 +38,7 @@ createTrainingProject <- function(name,
 
   # Handouts prep
   if ("handouts" %in% dirs) {
-    devtools:::add_desc_package(name, "Imports", handoutEngine)
+    devtools::use_package(handoutEngine, pkg=name)
 
     if (handoutEngine != "rmarkdown") {
       message(paste(handoutEngine, "demo added"))
@@ -53,7 +54,7 @@ createTrainingProject <- function(name,
 
   # Slides prep
   if ("slides" %in% dirs) {
-    devtools:::add_desc_package(name, "Imports", slideEngine)
+    devtools::use_package(slideEngine, pkg=name)
 
     if (slideEngine != "rmarkdown") {
       message(paste(slideEngine, "demo added"))
@@ -67,9 +68,13 @@ createTrainingProject <- function(name,
 
   }
 
-  if (usePackrat) {
-    packrat::snapshot(name)
-    packrat::restore(name)
+  # Perform some basic packrat initialisation
+  if("packagedeps" %in% names(list(...))){
+    pkgdeps <- list(...)[["packagedeps"]]
+    if(pkgdeps == "packrat"){
+      packrat::snapshot(name)
+      packrat::restore(name)
+    }
   }
 
   invisible(TRUE)
