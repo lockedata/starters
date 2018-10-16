32847884441458fcde99e3aabaf2e1f8cb0aaad6
Jas Sohi <j450h1@gmail.com>
2018-10-16 01:47:10 -0700

    Use try catch to delete directory created if something fails later in the function createPackageProject

--- R/createPackageProject.R
+++ R/createPackageProject.R
@@ -19,16 +19,26 @@ createPackageProject <- function(name,
                                  coverage = c("codecov", "coveralls")) {
   devtools::create(name)
   if (bestPractices) {
-    devtools::use_travis(name)
-    devtools::use_code_of_conduct(name)
-    devtools::use_coverage(name, coverage)
-    devtools::use_mit_license(name)
-    use_news_md(name)
-    use_package_doc(name)
-    use_readme_rmd(name)
-    devtools::use_testthat(name)
-    devtools::use_vignette(name, name)
-    devtools::use_git(pkg = name)
+    tryCatch({
+      devtools::use_travis(name)
+      devtools::use_code_of_conduct(name)
+      devtools::use_coverage(name, coverage)
+      devtools::use_mit_license(name)
+      use_news_md(name)
+      use_package_doc(name)
+      use_readme_rmd(name)
+      devtools::use_testthat(name)
+      devtools::use_vignette(name, name)
+      devtools::use_git(pkg = name)
+    }
+    ,
+    error = function(e) {
+      e
+      # delete folder created earlier
+      unlink(name, recursive = TRUE)
+      print(sprintf("Oops! An error was found and the directory `%s` was deleted", name))
+    }
+    )
   }
   invisible(TRUE)
 }
