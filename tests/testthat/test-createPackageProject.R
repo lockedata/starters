882ffa7ada96e0dc9b10c7883799f59ed0da1dbe
Maëlle Salmon <maelle.salmon@yahoo.se>
2018-11-01 09:39:55 +0100

    rm try() from tests fix #60

--- tests/testthat/test-createPackageProject.R
+++ tests/testthat/test-createPackageProject.R
@@ -22,23 +22,22 @@ project_name <- "packageProject2"
 
 test_that("createPackageProject() creates as expected when using defaults",{
 
-  proj <- try(createPackageProject(project_name))
-  if(!inherits(proj, 'try-error')){
+  createPackageProject(project_name)
     #expect_true(file.exists(file.path(project_name, paste0(project_name, ".Rproj"))))
     #expect_true(file.exists(file.path(project_name, "codecov.yml")))
-    expect_true(file.exists(file.path(project_name, "CODE_OF_CONDUCT.md")))
-    expect_true(file.exists(file.path(project_name, "DESCRIPTION")))
-    expect_true(file.exists(file.path(project_name, "LICENSE")))
-    expect_true(file.exists(file.path(project_name, "NAMESPACE")))
-    expect_true(file.exists(file.path(project_name, "NEWS.md")))
-    expect_true(file.exists(file.path(project_name, "R")))
-    expect_true(file.exists(file.path(project_name, "README.Rmd")))
-    expect_true(file.exists(file.path(project_name, "tests")))
-    expect_true(file.exists(file.path(project_name, "vignettes")))
-    expect_true(file.exists(file.path(project_name, ".git")))
-    expect_true(file.exists(file.path(project_name, ".gitignore")))
-    expect_true(file.exists(file.path(project_name, ".travis.yml")))
-  }
+  expect_true(file.exists(file.path(project_name, "CODE_OF_CONDUCT.md")))
+  expect_true(file.exists(file.path(project_name, "DESCRIPTION")))
+  expect_true(file.exists(file.path(project_name, "LICENSE")))
+  expect_true(file.exists(file.path(project_name, "NAMESPACE")))
+  expect_true(file.exists(file.path(project_name, "NEWS.md")))
+  expect_true(file.exists(file.path(project_name, "R")))
+  expect_true(file.exists(file.path(project_name, "README.Rmd")))
+  expect_true(file.exists(file.path(project_name, "tests")))
+  expect_true(file.exists(file.path(project_name, "vignettes")))
+  expect_true(file.exists(file.path(project_name, ".git")))
+  expect_true(file.exists(file.path(project_name, ".gitignore")))
+  expect_true(file.exists(file.path(project_name, ".travis.yml")))
+
 })
 
 unlink(project_name, recursive = TRUE, force = TRUE)
--- tests/testthat/test-createTrainingProject.R
+++ tests/testthat/test-createTrainingProject.R
@@ -16,68 +16,59 @@ project_name <- "trainingProject2"
 test_that("createTrainingProject() creates as expected when using defaults",{
 
 
-  proj <- try(createTrainingProject(project_name))
+  createTrainingProject(project_name)
+  expect_true(file.exists(file.path(project_name, paste0(project_name, ".Rproj"))))
+  expect_true(file.exists(file.path(project_name, "DESCRIPTION")))
+  expect_true(file.exists(file.path(project_name, "R")))
+  expect_true(file.exists(file.path(project_name, "README.Rmd")))
+  expect_true(file.exists(file.path(project_name, ".git")))
+  expect_true(file.exists(file.path(project_name, ".gitignore")))
+  expect_true(file.exists(file.path(project_name, ".travis.yml")))
+  expect_true(file.exists(file.path(project_name, "data")))
+  expect_true(file.exists(file.path(project_name, "handouts")))
+  expect_true(file.exists(file.path(project_name, "slides")))
 
-  if(!inherits(proj, 'try-error')){
-    expect_true(file.exists(file.path(project_name, paste0(project_name, ".Rproj"))))
-    expect_true(file.exists(file.path(project_name, "DESCRIPTION")))
-    expect_true(file.exists(file.path(project_name, "R")))
-    expect_true(file.exists(file.path(project_name, "README.Rmd")))
-    expect_true(file.exists(file.path(project_name, ".git")))
-    expect_true(file.exists(file.path(project_name, ".gitignore")))
-    expect_true(file.exists(file.path(project_name, ".travis.yml")))
-    expect_true(file.exists(file.path(project_name, "data")))
-    expect_true(file.exists(file.path(project_name, "handouts")))
-    expect_true(file.exists(file.path(project_name, "slides")))
-  }
 })
 
 unlink(project_name, recursive = TRUE, force = TRUE)
 
 test_that("createTrainingProject() creates as expected when using bookdown and revealjs",{
 
-  proj <- try(
-    createTrainingProject(project_name,
+  createTrainingProject(project_name,
       handoutEngine = "bookdown", slideEngine = "revealjs",
       packrat = FALSE)
-    )
-  if(!inherits(proj, 'try-error')){
-    expect_true(file.exists(file.path(project_name, paste0(project_name, ".Rproj"))))
-    expect_true(file.exists(file.path(project_name, "DESCRIPTION")))
-    expect_true(file.exists(file.path(project_name, "R")))
-    expect_true(file.exists(file.path(project_name, "README.Rmd")))
-    expect_true(file.exists(file.path(project_name, ".git")))
-    expect_true(file.exists(file.path(project_name, ".gitignore")))
-    expect_true(file.exists(file.path(project_name, ".travis.yml")))
-    expect_true(file.exists(file.path(project_name, "data")))
-    expect_true(file.exists(file.path(project_name, "handouts")))
-    expect_true(file.exists(file.path(project_name, "handouts", "index.Rmd")))
-    expect_true(file.exists(file.path(project_name, "slides", "revealjs_slides.Rmd")))
-  }
+  expect_true(file.exists(file.path(project_name, paste0(project_name, ".Rproj"))))
+  expect_true(file.exists(file.path(project_name, "DESCRIPTION")))
+  expect_true(file.exists(file.path(project_name, "R")))
+  expect_true(file.exists(file.path(project_name, "README.Rmd")))
+  expect_true(file.exists(file.path(project_name, ".git")))
+  expect_true(file.exists(file.path(project_name, ".gitignore")))
+  expect_true(file.exists(file.path(project_name, ".travis.yml")))
+  expect_true(file.exists(file.path(project_name, "data")))
+  expect_true(file.exists(file.path(project_name, "handouts")))
+  expect_true(file.exists(file.path(project_name, "handouts", "index.Rmd")))
+  expect_true(file.exists(file.path(project_name, "slides", "revealjs_slides.Rmd")))
+
 })
 
 unlink(project_name, recursive = TRUE, force = TRUE)
 
 test_that("createTrainingProject() creates as expected when using tufte and xaringan",{
-
-  proj <- try(
-    createTrainingProject(project_name,
+ createTrainingProject(project_name,
       handoutEngine = "tufte", slideEngine = "xaringan", packagedeps = "none")
-    )
 
-  if(!inherits(proj, 'try-error')){
-    expect_true(file.exists(file.path(project_name, paste0(project_name, ".Rproj"))))
-    expect_true(file.exists(file.path(project_name, "DESCRIPTION")))
-    expect_true(file.exists(file.path(project_name, "R")))
-    expect_true(file.exists(file.path(project_name, "README.Rmd")))
-    expect_true(file.exists(file.path(project_name, ".git")))
-    expect_true(file.exists(file.path(project_name, ".gitignore")))
-    expect_true(file.exists(file.path(project_name, ".travis.yml")))
-    expect_true(file.exists(file.path(project_name, "data")))
-    expect_true(file.exists(file.path(project_name, "handouts")))
-    expect_true(file.exists(file.path(project_name, "handouts", "tufte_handout.Rmd")))
-    expect_true(file.exists(file.path(project_name, "slides", "xaringan_slides.Rmd")))
-  }
+  expect_true(file.exists(file.path(project_name, paste0(project_name, ".Rproj"))))
+  expect_true(file.exists(file.path(project_name, "DESCRIPTION")))
+  expect_true(file.exists(file.path(project_name, "R")))
+  expect_true(file.exists(file.path(project_name, "README.Rmd")))
+  expect_true(file.exists(file.path(project_name, ".git")))
+  expect_true(file.exists(file.path(project_name, ".gitignore")))
+  expect_true(file.exists(file.path(project_name, ".travis.yml")))
+  expect_true(file.exists(file.path(project_name, "data")))
+  expect_true(file.exists(file.path(project_name, "handouts")))
+  expect_true(file.exists(file.path(project_name, "handouts", "tufte_handout.Rmd")))
+  expect_true(file.exists(file.path(project_name, "slides", "xaringan_slides.Rmd")))
+
 })
 
 unlink(project_name, recursive = TRUE, force = TRUE)
