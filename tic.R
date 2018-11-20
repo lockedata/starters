3c4ebec7abdad9e831b93b51645a30170241186a
Maëlle Salmon <maelle.salmon@yahoo.se>
2018-11-20 15:43:01 +0100

    install spelling

--- tic.R
+++ tic.R
@@ -14,6 +14,7 @@ if (Sys.getenv("id_rsa") != "") {
     get_stage("deploy") %>%
       add_code_step(covr::codecov()) %>%
       add_code_step(devtools::install()) %>%
+      add_code_step(install.packages("spelling")) %>%
       add_code_step(covrpage::covrpage_ci()) %>%
       add_step(step_push_deploy(commit_paths = "tests/README.md")) %>%
       add_step(step_build_pkgdown()) %>%
