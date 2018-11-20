af6b73597725cf26e85b3c1464d6f978e739c433
Maëlle Salmon <maelle.salmon@yahoo.se>
2018-11-20 15:29:39 +0100

    covrpage and health fixes

--- tic.R
+++ tic.R
@@ -10,20 +10,22 @@ if (Sys.getenv("id_rsa") != "") {
   get_stage("before_deploy") %>%
     add_step(step_setup_ssh())
 
-  if (ci()$get_branch() == "master" || ci()$is_tag()) {
-  get_stage("deploy")  %>%
+  if (ci()$get_branch() == "master") {
+    get_stage("deploy") %>%
       add_code_step(covr::codecov()) %>%
       add_code_step(devtools::install()) %>%
       add_code_step(covrpage::covrpage_ci()) %>%
+      add_step(step_push_deploy(commit_paths = "tests/README.md")) %>%
       add_step(step_build_pkgdown()) %>%
       add_step(step_push_deploy(path = "docs", branch = "gh-pages")) %>%
       add_code_step(library("pRojects"))%>%
       add_code_step(get_project_health()) %>%
       add_step(step_push_deploy(path = "health", branch = "project-health"))
   }else{
-    get_stage("deploy")  %>%
+    get_stage("deploy") %>%
       add_code_step(covr::codecov()) %>%
       add_code_step(devtools::install()) %>%
-      add_code_step(covrpage::covrpage_ci())
+      add_code_step(covrpage::covrpage_ci()) %>%
+      add_step(step_push_deploy(commit_paths = "tests/README.md"))
   }
 }
