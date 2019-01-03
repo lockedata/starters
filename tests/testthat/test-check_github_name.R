fdd753b1dbbaee438c3a002f53ce96e83b9b4d97
Maëlle Salmon <maelle.salmon@yahoo.se>
2019-01-03 18:04:15 +0100

    rename package to starters

--- tests/testthat/test-check_github_name.R
+++ tests/testthat/test-check_github_name.R
@@ -1,7 +1,7 @@
 context("test-check_github_name")
 
 test_that("check_github_name works", {
-  expect_error(check_github_name("lockedata", "pRojects"))
-  expect_silent(check_github_name(NULL, "pRojects"))
+  expect_error(check_github_name("lockedata", "cransays"))
+  expect_silent(check_github_name(NULL, "starters"))
   expect_silent(check_github_name("maelle", "pRRRojects"))
 })
