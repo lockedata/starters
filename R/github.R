1944cc705cf29b268fabf31949d01666940c7e45
Maëlle Salmon <maelle.salmon@yahoo.se>
2019-01-17 12:22:23 +0100

    :cry:

--- R/github.R
+++ R/github.R
@@ -73,7 +73,8 @@ setup_gh_repo <- function(username, private, protocol,
   )
   git2r::remote_add(r, "origin", origin_url)
   git2r::checkout(r, "master")
-  git2r::branch_set_upstream(git2r::repository_head(r), "origin/master")
+  # not possible cf https://github.com/ropensci/git2r/issues/133
+  #git2r::branch_set_upstream(git2r::repository_head(r), "origin/master")
   desc::desc_set("URL", create$html_url,
     file = usethis::proj_get()
   )
