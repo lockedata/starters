e111db27f475b4d62866e61b2dde817cdc496107
Maëlle Salmon <maelle.salmon@yahoo.se>
2019-01-24 13:49:42 +0100

    less stuff in tic

--- inst/templates/tic/package/tic.R
+++ inst/templates/tic/package/tic.R
@@ -4,11 +4,6 @@ get_stage("after_success") %>%
   add_code_step(covr::{{coverage_service}}(quiet = FALSE))
 
 if (Sys.getenv("id_rsa") != "") {
-  # pkgdown documentation can be built optionally. Other example criteria:
-  # - `inherits(ci(), "TravisCI")`: Only for Travis CI
-  # - `ci()$is_tag()`: Only for tags, not for branches
-  # - `Sys.getenv("BUILD_PKGDOWN") != ""`: If the env var "BUILD_PKGDOWN" is set
-  # - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
   get_stage("before_deploy") %>%
     add_step(step_setup_ssh())
 
@@ -16,21 +11,11 @@ if (Sys.getenv("id_rsa") != "") {
     get_stage("deploy") %>%
       add_code_step(covr::{{coverage_service}}()) %>%
       add_code_step(remotes::install("lockedata/starters")) %>%
-      add_code_step(remotes::install("metrumresearchgroup/covrpage")) %>%
       add_code_step(install.packages("spelling")) %>%
-      add_code_step(covrpage::covrpage_ci()) %>%
-      add_step(step_push_deploy(commit_paths = "tests/README.md")) %>%
       add_step(step_build_pkgdown()) %>%
       add_step(step_push_deploy(path = "docs", branch = "gh-pages")) %>%
-      add_code_step(library("pRojects"))%>%
+      add_code_step(library("starters"))%>%
       add_code_step(get_project_health()) %>%
       add_step(step_push_deploy(path = "health", branch = "project-health"))
-  }else{
-    get_stage("deploy") %>%
-      add_code_step(covr::{{coverage_service}}()) %>%
-      add_code_step(devtools::install()) %>%
-      add_code_step(remotes::install("metrumresearchgroup/covrpage")) %>%
-      add_code_step(covrpage::covrpage_ci()) %>%
-      add_step(step_push_deploy(commit_paths = "tests/README.md"))
   }
 }
--- tic.R
+++ tic.R
@@ -4,11 +4,7 @@ get_stage("after_success") %>%
   add_code_step(covr::coveralls(quiet = FALSE))
 
 if (Sys.getenv("id_rsa") != "") {
-  # pkgdown documentation can be built optionally. Other example criteria:
-  # - `inherits(ci(), "TravisCI")`: Only for Travis CI
-  # - `ci()$is_tag()`: Only for tags, not for branches
-  # - `Sys.getenv("BUILD_PKGDOWN") != ""`: If the env var "BUILD_PKGDOWN" is set
-  # - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
+
   get_stage("before_deploy") %>%
     add_step(step_setup_ssh())
 
