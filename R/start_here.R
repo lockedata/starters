3cd3fee413a3dc785cf3ae77efd49d99fb986f6b
Maëlle Salmon <maelle.salmon@yahoo.se>
2019-02-11 15:34:46 +0100

    add ...

--- R/start_here.R
+++ R/start_here.R
@@ -53,7 +53,7 @@ start_here <- function(){
   }
 
 
-  usethis::ui_line("Checking build tools are available")
+  usethis::ui_line("Checking build tools are available...")
   if(!pkgbuild::has_build_tools()){
     browseURL("https://usethis.r-lib.org/articles/articles/usethis-setup.html#prepare-your-system-to-build-packages-from-source")
     usethis::ui_stop("Your system lacks a few tools, see link opened. https://usethis.r-lib.org/articles/articles/usethis-setup.html#prepare-your-system-to-build-packages-from-source")
