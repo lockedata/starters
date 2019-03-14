6de983db04b7b749c2cf3515bd7469a6e86283b2
Maëlle Salmon <maelle.salmon@yahoo.se>
2019-03-14 08:13:37 +0100

    fix #130

--- R/start_here.R
+++ R/start_here.R
@@ -47,14 +47,14 @@ start_here <- function(){
 
   usethis::ui_line("Checking default description values...")
   if(is.null(getOption("usethis.description"))){
-    usethis::ui_code_block(options(
-      usethis.description = list(
-        `Authors@R` = 'person("Jane", "Doe", email = "jane@example.com", role = c("aut", "cre"),
-                          comment = c(ORCID = "YOUR-ORCID-ID"))',
-        License = "MIT + file LICENSE",
-        Language =  "es"
-      )
-    ))
+    usethis::ui_code_block("options(
+    usethis.description = list(
+      `Authors@R` = 'person(\"Jane\", \"Doe\", email = \"jane@example.com\", role = c(\"aut\", \"cre\"),
+      comment = c(ORCID = \"YOUR-ORCID-ID\"))',
+      License = \"MIT + file LICENSE\",
+      Language =  \"es\"
+  )
+  )")
 
     usethis::ui_todo("DESCRIPTION defaults not set yet. Amend and paste the code above (copied to clipboard) to your .Rprofile via running usethis::edit_r_profile().")
     usethis::ui_stop("Set DESCRIPTION default then run starters::start_here() again.")
