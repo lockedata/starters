b11ca483a6313161e42f6574d000fea15b689e03
Jon Calder <jonmcalder@gmail.com>
2019-04-03 19:13:03 +0200

    Add styler to Suggests when adding pre-commit hook

--- R/styler_precommit.R
+++ R/styler_precommit.R
@@ -1,16 +1,23 @@
-add_styler_hook <- function(){
-  fs::dir_create(file.path(usethis::proj_get(),
-                           ".git", "hooks"))
-
-
-  fs::file_copy(system.file("stylerprecommit.sh",
-                            package = "starters"),
-                file.path(usethis::proj_get(),
-                          ".git", "hooks",
-                          "pre-commit"))
-  fs::file_copy(system.file("precommit.R",
-                            package = "starters"),
-                file.path(usethis::proj_get(),
-                          ".git", "hooks",
-                          "precommit.R"))
+add_styler_hook <- function() {
+  fs::dir_create(file.path(
+    usethis::proj_get(),
+    ".git", "hooks"
+  ))
+  fs::file_copy(
+    system.file("stylerprecommit.sh", package = "starters"),
+    file.path(
+      usethis::proj_get(),
+      ".git", "hooks",
+      "pre-commit"
+    )
+  )
+  fs::file_copy(
+    system.file("precommit.R", package = "starters"),
+    file.path(
+      usethis::proj_get(),
+      ".git", "hooks",
+      "precommit.R"
+    )
+  )
+  usethis::use_package("styler", type = "Suggests")
 }
