b5153108418ca5c557c269630495cc1df816b450
Maëlle Salmon <maelle.salmon@yahoo.se>
2019-12-09 19:30:15 +0100

    try

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -3,3 +3,17 @@ library("tufte")
 library("bookdown")
 library("xaringan")
 library("revealjs")
+
+# adapted from https://github.com/r-lib/usethis/blob/master/tests/testthat/helper.R
+
+temp_folder <- function(pattern) {
+  tmp <- tempfile(
+    pattern = "aaa",
+    tempdir(check = TRUE)
+  )
+  fs::dir_create(tmp)
+  withr::defer({
+    fs::dir_delete(tmp)
+  }, envir = parent.frame())
+  return(tmp)
+}
